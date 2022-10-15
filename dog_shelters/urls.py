fe55e9865d76cbd926957a582caef7d5dd1309eb
Nathan Bangwa <nathanbangwa7@gmail.com>
2022-10-15 16:48:24 +0200

    update shelter view to view dog details

--- dog_shelters/urls.py
+++ dog_shelters/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.shelter_list, name='shelter_list'),
     path('shelter/<int:pk>', views.shelter_detail, name='shelter_detail'),
+    path('dog/<int:pk>', views.DogDetailView.as_view(), name='dog_detail'),
+
     # More patterns to come later
 ]
